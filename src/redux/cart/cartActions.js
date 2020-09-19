1ca79a2d415231147e4f6f5e219e985c91aa3b17
Web Dev <tom2kota@gmail.com>
2020-09-20 01:30:58 +0300

    Add remove item feature to Checkout item component

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -1,4 +1,4 @@
-import {ADD_ITEM, TOGGLE_CART_HIDDEN} from "./cartTypes";
+import {ADD_ITEM, CLEAR_ITEM_FROM_CART, TOGGLE_CART_HIDDEN} from "./cartTypes";
 
 export const toggleCartHidden = () => ({
     type: TOGGLE_CART_HIDDEN
@@ -6,5 +6,10 @@ export const toggleCartHidden = () => ({
 
 export const addItem = item => ({
     type: ADD_ITEM,
+    payload: item
+})
+
+export const clearItemFromCart = item => ({
+    type: CLEAR_ITEM_FROM_CART,
     payload: item
 })
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,4 +1,4 @@
-import {ADD_ITEM, TOGGLE_CART_HIDDEN} from "./cartTypes";
+import {ADD_ITEM, CLEAR_ITEM_FROM_CART, TOGGLE_CART_HIDDEN} from "./cartTypes";
 import {addItemToCart} from "./cartUtils";
 
 const INITIAL_STATE = {
@@ -17,6 +17,13 @@ export const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 // cartItems: [...state.cartItems, action.payload],
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
             }
         default:
             return state
