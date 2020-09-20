12a8397fe823b6df589bb38c976d0d18f1e6c5e9
Web Dev <tom2kota@gmail.com>
2020-09-20 14:42:40 +0300

    Move Directory data to separate file

--- src/redux/directory/directotyReducer.js
+++ src/redux/directory/directotyReducer.js
@@ -1,44 +1,4 @@
-import categoryOneImage from "../../images/01.png";
-import categoryTwoImage from "../../images/03.png";
-import categoryThreeImage from "../../images/10.png";
-import categoryFourImage from "../../images/11.png";
-import categoryFiveImage from "../../images/09.png";
-
-const INITIAL_STATE = {
-    sections: [
-        {
-            id: 1,
-            title: 'category #1',
-            image: categoryOneImage,
-            linkUrl: 'category_1'
-        },
-        {
-            id: 2,
-            title: 'category #2',
-            image: categoryTwoImage,
-            linkUrl: 'category_2'
-        },
-        {
-            id: 3,
-            title: 'category #3',
-            image: categoryThreeImage,
-            linkUrl: 'category_3'
-        },
-        {
-            id: 4,
-            title: 'category #4',
-            image: categoryFourImage,
-            linkUrl: 'category_4'
-        },
-        {
-            id: 5,
-            title: 'category #5',
-            image: categoryFiveImage,
-            linkUrl: 'category_5'
-        },
-
-    ]
-}
+import {INITIAL_STATE} from "./directoryData";
 
 export const directoryReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
