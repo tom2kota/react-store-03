31dd6858acf60d2e844fd6e821a917f5f7279543
Web Dev <tom2kota@gmail.com>
2020-09-20 14:37:51 +0300

    Move Directory data from component to reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {userReducer} from "./user/userReducer";
 import {cartReducer} from "./cart/cartReducer";
+import {directoryReducer} from "./directory/directotyReducer";
 
 export default combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 })
